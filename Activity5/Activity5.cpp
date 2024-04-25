8e9f583c9d87f654ddfd3a1638f65e9486583a6b
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-04-25 11:37:09 +0700

    membuat catch dan while

--- Activity5/Activity5.cpp
+++ Activity5/Activity5.cpp
@@ -177,3 +177,9 @@ int main()
                 break;
             }
         }
+        catch (exception e)
+        {
+            cout << "Terjadi kesalahan" << endl;
+        }
+    } while (pilihan != 5);
+}
